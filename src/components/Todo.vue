1efe66f9e7b68f0e4701d4fb35f02568cca6c0d6
dreyhiflden <dreyhiflden@gmail.com>
2019-10-22 15:46:40 +0300

    Include Vuex and SCSS to project, create initial component

---
+++ src/components/Todo.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>Text</div>
+</template>
+
+<script>
+export default {
+  name: "Todo",
+  data: function () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
