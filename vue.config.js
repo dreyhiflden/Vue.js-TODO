cca24b7ded370d6969478e8eb73192b079a1e756
dreyhiflden <dreyhiflden@gmail.com>
2019-10-25 06:31:55 +0300

    Minor changes

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 // vue.config.js
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/todotestcase/dist/'
+    ? '/skd-testcase/dist/'
     : '/',
   runtimeCompiler: true,
 }
